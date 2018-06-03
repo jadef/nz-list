c44a693b4f99f14bbb4f84af099fd2a884009092
jadef <jade@jfaist.com>
2018-06-03 10:35:13 -0700

    remove unused + claimed + visual layout updates

--- app/js/components/entries/Entry.jsx
+++ app/js/components/entries/Entry.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // -- Components
-import Rating from './Rating';
 import Tags from './Tags';
 
 // -- Styles
@@ -13,42 +12,28 @@ import '../../../sass/components/entries/entry.scss';
     - Main
       - Entries
         - *Entry*
-          - Rating
-            - Star
           - Tags
             - Tag
 */
 
 class Entry extends React.Component {
 
-  checkLetter = () => {
-    // If passed a new jump
-    if (this.props.newLetter) {
-      const letter = this.props.entry.id.charAt(0).toUpperCase();
-      return (
-        "jump"+letter
-      );
-    }
-  }
-
   render() {
     const entry = this.props.entry;
 
     return (
-      <section className="entry" id={this.checkLetter()}>
+      <section className={(entry.claimed ? 'entry claimed' : 'entry')} >
         <div className="title" id={entry.id}>
           <h3>{entry.title}</h3>
-          {entry.link ? <a className="external" href={entry.link} target="_blank"><img src="images/external.svg" alt="external link" /></a> : null}
         </div>
-        <Rating rank={entry.rank} />
         <div className="desc">
+          <div className="img"><img src="https://picsum.photos/600/400" /></div>
           <div className="content">
-            <div className="notes">{entry.notes}</div>
             <div dangerouslySetInnerHTML={ {__html: entry.content} } />
+            <div className="notes">{entry.notes}</div>
           </div>
           {entry.tags ? <Tags tags={entry.tags} activeTags={this.props.activeTags} /> : null}
         </div>
-        <div className="stamp">edited: {entry.stamp}</div>
       </section>
     );
   }
